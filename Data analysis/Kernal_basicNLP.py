2b785096b4b34cd7fd3309bd33e36f97b849a823
jxtang0920 <jiaxin.tang2016@outlook.com>
2017-10-25 16:58:54 -0400

    Data part

---
+++ Data analysis/Kernal_basicNLP.py
@@ -0,0 +1,34 @@
+%matplotlib inline
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import log_loss, accuracy_score
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+from sklearn.decomposition import TruncatedSVD
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+
+import gensim
+
+import scikitplot.plotters as skplt
+
+import nltk
+
+from xgboost import XGBClassifier
+
+import os
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, LSTM
+from keras.utils.np_utils import to_categorical
+from keras.callbacks import ModelCheckpoint
+from keras.models import load_model
+from keras.optimizers import Adam
