3a0901d9de1fcedea0bed7e26aa91e41a805d3c4
jxtang0920 <jiaxin.tang2016@outlook.com>
2017-10-28 22:22:40 -0400

    upload

--- Data analysis/Kernal_basicNLP.py
+++ Data analysis/Kernal_basicNLP.py
@@ -32,3 +32,40 @@ from keras.utils.np_utils import to_categorical
 from keras.callbacks import ModelCheckpoint
 from keras.models import load_model
 from keras.optimizers import Adam
+
+df_train_txt = pd.read_csv('training_text', sep='\|\|', header=None, skiprows=1, names=["ID","Text"])
+df_train_var = pd.read_csv('training_variants')
+df_test_txt = pd.read_csv('test_text', sep='\|\|', header=None, skiprows=1, names=["ID","Text"])
+df_test_var = pd.read_csv('test_variants')
+
+df_train = pd.merge(df_train_var, df_train_txt, how='left', on='ID')
+df_test = pd.merge(df_test_var, df_test_txt, how='left', on='ID')
+
+df_train['Class'].value_counts().plot(kind="bar", rot=0)
+
+# This cell reduces the training data for Kaggle limits. Remove this cell for real results.
+df_train, _ = train_test_split(df_train, test_size=0.7, random_state=8, stratify=df_train['Class'])
+
+def evaluate_features(X, y, clf=None):
+    """General helper function for evaluating effectiveness of passed features in ML model
+    
+    Prints out Log loss, accuracy, and confusion matrix with 3-fold stratified cross-validation
+    
+    Args:
+        X (array-like): Features array. Shape (n_samples, n_features)
+        
+        y (array-like): Labels array. Shape (n_samples,)
+        
+        clf: Classifier to use. If None, default Log reg is use.
+    """
+    if clf is None:
+        clf = LogisticRegression()
+    
+    probas = cross_val_predict(clf, X, y, cv=StratifiedKFold(random_state=8), 
+                              n_jobs=-1, method='predict_proba', verbose=2)
+    pred_indices = np.argmax(probas, axis=1)
+    classes = np.unique(y)
+    preds = classes[pred_indices]
+    print('Log loss: {}'.format(log_loss(y, probas)))
+    print('Accuracy: {}'.format(accuracy_score(y, preds)))
+    skplt.plot_confusion_matrix(y, preds)
