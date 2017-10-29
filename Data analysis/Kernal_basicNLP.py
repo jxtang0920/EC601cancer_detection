74dd72b6eb4cf5e1a9d7adb05aa4bed0d2148ab5
jxtang0920 <jiaxin.tang2016@outlook.com>
2017-10-29 00:02:09 -0400

    a

--- Data analysis/Kernal_basicNLP.py
+++ Data analysis/Kernal_basicNLP.py
@@ -1,4 +1,3 @@
-%matplotlib inline
 import pandas as pd
 import numpy as np
 
@@ -41,6 +40,8 @@ df_test_var = pd.read_csv('test_variants')
 df_train = pd.merge(df_train_var, df_train_txt, how='left', on='ID')
 df_test = pd.merge(df_test_var, df_test_txt, how='left', on='ID')
 
+#df_train.describe(include='all')
+#df_test.describe(include='all')
 df_train['Class'].value_counts().plot(kind="bar", rot=0)
 
 # This cell reduces the training data for Kaggle limits. Remove this cell for real results.
@@ -69,3 +70,130 @@ def evaluate_features(X, y, clf=None):
     print('Log loss: {}'.format(log_loss(y, probas)))
     print('Accuracy: {}'.format(accuracy_score(y, preds)))
     skplt.plot_confusion_matrix(y, preds)
+#test
+"""    
+from sklearn.datasets import load_iris
+evaluate_features(*load_iris(True))
+"""
+count_vectorizer = CountVectorizer(
+    analyzer="word", tokenizer=nltk.word_tokenize,
+    preprocessor=None, stop_words='english', max_features=None)    
+
+bag_of_words = count_vectorizer.fit_transform(df_train['Text'])
+len(count_vectorizer.get_feature_names())
+
+"""TruncatedSVD"""
+svd = TruncatedSVD(n_components=25, n_iter=25, random_state=12)
+truncated_bag_of_words = svd.fit_transform(bag_of_words)
+
+'''raw_testcases
+evaluate_features(truncated_bag_of_words, df_train['Class'].values.ravel())
+evaluate_features(truncated_bag_of_words, df_train['Class'].values.ravel(), 
+                  RandomForestClassifier(n_estimators=1000, max_depth=5, verbose=1))
+evaluate_features(tfidf, df_train['Class'].values.ravel(), 
+                  SVC(kernel='linear', probability=True))
+'''
+
+"""get word2vec"""
+class MySentences(object):
+    """MySentences is a generator to produce a list of tokenized sentences 
+    
+    Takes a list of numpy arrays containing documents.
+    
+    Args:
+        arrays: List of arrays, where each element in the array contains a document.
+    """
+    def __init__(self, *arrays):
+        self.arrays = arrays
+ 
+    def __iter__(self):
+        for array in self.arrays:
+            for document in array:
+                for sent in nltk.sent_tokenize(document):
+                    yield nltk.word_tokenize(sent)
+
+def get_word2vec(sentences, location):
+    """Returns trained word2vec
+    
+    Args:
+        sentences: iterator for sentences
+        
+        location (str): Path to save/load word2vec
+    """
+    if os.path.exists(location):
+        print('Found {}'.format(location))
+        model = gensim.models.Word2Vec.load(location)
+        return model
+    
+    print('{} not found. training model'.format(location))
+    model = gensim.models.Word2Vec(sentences, size=100, window=5, min_count=5, workers=4)
+    print('Model done training. Saving to disk')
+    model.save(location)
+    return model
+
+"""training the word2vec model.word2vec training is unsupervised, can use both training and test datasets."""
+w2vec = get_word2vec(
+    MySentences(
+        df_train['Text'].values, 
+        #df_test['Text'].values  
+    ),
+    'w2vmodel'
+)
+    
+class MyTokenizer:
+    def __init__(self):
+        pass
+    
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        transformed_X = []
+        for document in X:
+            tokenized_doc = []
+            for sent in nltk.sent_tokenize(document):
+                tokenized_doc += nltk.word_tokenize(sent)
+            transformed_X.append(np.array(tokenized_doc))
+        return np.array(transformed_X)
+    
+    def fit_transform(self, X, y=None):
+        return self.transform(X)
+
+class MeanEmbeddingVectorizer(object):
+    def __init__(self, word2vec):
+        self.word2vec = word2vec
+        # if a text is empty we should return a vector of zeros
+        # with the same dimensionality as all the other vectors
+        self.dim = len(word2vec.wv.syn0[0])
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X = MyTokenizer().fit_transform(X)
+        
+        return np.array([
+            np.mean([self.word2vec.wv[w] for w in words if w in self.word2vec.wv]
+                    or [np.zeros(self.dim)], axis=0)
+            for words in X
+        ])
+    
+    def fit_transform(self, X, y=None):
+        return self.transform(X)
+
+mean_embedding_vectorizer = MeanEmbeddingVectorizer(w2vec)
+mean_embedded = mean_embedding_vectorizer.fit_transform(df_train['Text'])
+
+'''word2vec_testcases
+evaluate_features(mean_embedded, df_train['Class'].values.ravel())
+evaluate_features(mean_embedded, df_train['Class'].values.ravel(),
+                  RandomForestClassifier(n_estimators=1000, max_depth=15, verbose=1))
+evaluate_features(mean_embedded, 
+                  df_train['Class'].values.ravel(),
+                  XGBClassifier(max_depth=4,
+                                objective='multi:softprob',
+                                learning_rate=0.03333,
+                                )
+                 )
+'''
+
